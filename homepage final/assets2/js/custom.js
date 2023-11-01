64110ebfeba14889188f8bf44e3010679b14c3e8
ritikabaj <ritikab.2022@scis.smi.edu.sg>
2023-11-02 01:27:18 +0800

    minor changes

--- homepage final/assets2/js/custom.js
+++ homepage final/assets2/js/custom.js
@@ -95,10 +95,12 @@
       $('.nav a').each(function () {
           var currLink = $(this);
           var refElement = $(currLink.attr("href"));
+          if (refElement && refElement.length > 0) {
           if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
               $('.nav ul li a').removeClass("active");
               currLink.addClass("active");
           }
+        }
           else{
               currLink.removeClass("active");
           }
